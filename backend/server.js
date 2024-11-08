8ed3b74d3acc663239683c43f5d4ca776b18d8e8
gowtham <gowthamv0210@gmail.com>
2024-11-08 22:48:24 +0530

    organized files

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const routes = require("./routes/recipeRoutes");
+const port = process.env.PORT || 3001;
+const cors = require("cors");
+
+app.use(cors());
+require("dotenv").config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//const expressLayout = require("express-ejs-layouts");
+//app.use(expressLayout);
+//app.set("layout", "./layouts/main");
+
+app.use(routes);
+
+app.listen(port, () => console.log(`Listening to port ${port}`));
